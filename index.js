02cd960b9e6da9c802a093058f1e310ef71cf03c
Jascha Biederstedt <j.biederstedt@gmx.de>
2020-11-08 21:20:31 +0100

    Implement reusable debounce

--- index.js
+++ index.js
@@ -11,15 +11,10 @@ const fetchData = async (searchTerm) => {
   console.log(response.data);
 }
 
-let timeoutId;
 const input = document.querySelector('input');
+
 const onInput = event => {
-  if(timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  timeoutId = setTimeout(() => {
     fetchData(event.target.value);
-  }, 1000);
 };
 
-input.addEventListener('input', onInput);
+input.addEventListener('input', debounce(onInput, 500));
